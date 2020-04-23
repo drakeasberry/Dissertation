9e4d6add94f24e29a76ab069a1540dd81f2cb11e
Drake Asberry <dasberry@email.arizona.edu>
2020-04-22 19:22:05 -0700

    wrote code needed to begin R analysis.

--- Statistics/Lexical_Access/lexical_access_stats.r
+++ Statistics/Lexical_Access/lexical_access_stats.r
@@ -14,9 +14,18 @@ library(tidyverse)
 options(readr.num_columns = 0)
 
 # get all csv files from directory
-lexical_dir = 'analyze_data/temp_data/segCols' #set path to directory
-lexical_paths = list.files(path=lexical_dir, pattern = '*.csv', full.names = TRUE) #list all the files with path
-print(lexical_paths) # prints path with filenames
-
+lexical_dir = 'analyze_data/raw' #set path to directory
 lexical_files <- list.files(path=lexical_dir, pattern = '*.csv', full.names = TRUE) #list all the files with path
-df_lexical_data_raw = ldply(lexical_files, read_csv)
+
+# read all csv files into a single tibble
+lexical_data_raw = ldply(lexical_files, read_csv)
+lexical_data <- lexical_data_raw %>%
+  mutate(prime_syl_structure = ifelse(numRevealed == 2, 'CV', 'CVC'),
+         match = ifelse(prime_syl_structure == word_initial_syl, 'match', 'mismatch'),
+         match_check = ifelse(matching == match, 'correct', 'error'),
+         stress_word_initial = ifelse(stress_word_initial == 'yes', 'stressed','unstressed')) %>%
+  select("partNum","lexicalRespCorr","lexicalRespRT","word","word_status","word_initial_syl","stress_word_initial","word_freq","Prime","prime_syl_structure","matching","expName","session","age","gender","birthCountry","placeResidence","education","preferLanguage","date")
+
+# write out to miquel workspace
+write_csv(lexical_data,'~/Desktop/working_diss_files/r-checking/lexical_data.csv')
+ 
