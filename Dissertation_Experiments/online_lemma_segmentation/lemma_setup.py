92b7dac68b1f71571b2eb6c706dd3b72bf8e1351
Drake Asberry <dasberry@email.arizona.edu>
2020-05-27 08:26:31 -0700

    Updated script to match correct file numbers and outputs to run in psychopy

--- Dissertation_Experiments/online_lemma_segmentation/lemma_setup.py
+++ Dissertation_Experiments/online_lemma_segmentation/lemma_setup.py
@@ -1,7 +1,27 @@
 import pandas as pd
+from Scripts_Dissertation import data_preparation
+
+# set project paths
+project_parent = 'Dissertation_Experiments/online_lemma_segmentation/'
+original_path = 'Dissertation_Experiments/online_lemma_segmentation/data/original_data/'
 processed_path = 'Dissertation_Experiments/online_lemma_segmentation/data/processed_data/'
-practice = pd.read_csv('Dissertation_Experiments/online_lemma_segmentation/data/original_data/exp_files/prac_trials.csv')
-experiment = pd.read_csv('Dissertation_Experiments/online_lemma_segmentation/data/original_data/exp_files/exp_trials.csv')
+experiment_file = 'exp_files/'
+participant_file = 'part_files/'
+
+# Create csv files for experiment
+# Enter filenames without extensions
+trial_list = ['16_prac_trials', '64_exp_trials']
+sheet_names = ['Sheet1']
+
+# Loop through all files to create csv outputs
+for trial in trial_list:
+    data_preparation.csv_from_excel(project_parent + trial + '.xlsx',
+                                    sheet_names[0],
+                                    original_path + experiment_file + trial + '.csv')
+
+# Read csv data files containing experimental items
+practice = pd.read_csv(original_path + experiment_file + '16_prac_trials.csv')
+experiment = pd.read_csv(original_path + experiment_file + '64_exp_trials.csv')
 
 def block_loop(df,dir,identifier):
     #print(dir)
@@ -12,5 +32,5 @@ def block_loop(df,dir,identifier):
         print(path)
         new_df.to_csv(dir + identifier + block + '.csv')
 
-block_loop(practice, processed_path + 'exp_files/','practice_')
-block_loop(experiment, processed_path + 'exp_files/','experiment_')
+block_loop(practice, processed_path + experiment_file,'practice_')
+block_loop(experiment, processed_path + experiment_file,'experiment_')
