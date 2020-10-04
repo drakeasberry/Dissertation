4ae550c9afc1a9f5934fce82e37339b49e966ebf
Drake Asberry <dasberry@email.arizona.edu>
2020-10-04 11:24:49 -0500

    Removed unecessary code and comments, and updated number of participants in file outputs.

--- Statistics/Lexical_Access/lexical_access_stats.r
+++ Statistics/Lexical_Access/lexical_access_stats.r
@@ -19,17 +19,20 @@ lexical_files <- list.files(path=lexical_dir, pattern = '*.csv', full.names = TR
 
 # read all csv files into a single tibble
 lexical_data_raw <- ldply(lexical_files, read_csv)
-write_csv(lexical_data_raw, 'all_58_lexical_items_raw.csv')
+#write_csv(lexical_data_raw, 'all_53_lexical_items_raw.csv')
 
+# Match returning participants to original participant number
 part_num_map <- read_csv('partNum_mapping.csv')
 part_num_map <- part_num_map[grep('part[0-9]+', part_num_map$Participated_E2),] %>%
   subset(Participated_E1 != 'Exp 2 Only')
 names(part_num_map)[2] = 'partNum'
 
+# Read in map to add language group information 
 group_map <- read_csv('../../Scripts_Dissertation/participant_group_map.csv')
 
+# Data cleanup for analysis
 lexical_data <- lexical_data_raw %>%
-  full_join(part_num_map, by= 'partNum') %>%
+  left_join(part_num_map, by= 'partNum') %>%
   left_join(group_map, by = 'partNum') %>% 
   mutate(partNum = ifelse(is.na(Participated_E1) == TRUE, partNum, Participated_E1),
          prime_syl_structure = ifelse(numRevealed == 2, 'CV', 'CVC'),
@@ -38,26 +41,18 @@ lexical_data <- lexical_data_raw %>%
          stress_word_initial = ifelse(stress_word_initial == 'yes', 'stressed','unstressed')) %>%
   select("partNum","group", "lexicalRespCorr","lexicalRespRT","word","word_status","word_initial_syl","stress_word_initial","word_freq","Prime","prime_syl_structure","matching","expName","session","age","gender","birthCountry","placeResidence","education","preferLanguage","date")
 
-
-# write out to miquel workspace
-write_csv(lexical_data, 'all_58_lexical_items_transformed.csv')
+# write out all lexical access participants
+#write_csv(lexical_data, 'all_53_lexical_items_transformed.csv')
 
 # drop heritage speaker group
-lexical_data_no_heritage <- subset(lexical_data, lexical_data$group != "Childhood")
-lexical_data_esp_part <- subset(lexical_data, lexical_data$group == "Spanish")
-lexical_data_eng_part <- subset(lexical_data, lexical_data$group == "English")
+lexical_data_no_heritage <- subset(lexical_data, lexical_data$group != "Childhood") %>% 
+  select(-c("expName","session","age","gender","birthCountry","placeResidence","education","preferLanguage"
+            ,"date"))
 
-lexical_data_no_heritage <- lexical_data_no_heritage %>% 
-  select(-c("expName","session","age","gender","birthCountry","placeResidence","education","preferLanguage","date"))
-write_csv(lexical_data_no_heritage, 'esp_eng_47_lexical_access.csv')
-#write_csv(lexical_data_no_heritage, 'data.csv') # For PI Advisor
+# Clean environment
+rm(group_map, part_num_map, lexical_data, lexical_data_raw, lexical_dir, lexical_files)
 
-# Need to create data set for demographics One line per participant
-# demogrpahics_all.csv
-# Create subset for Lexical Access Experiment Demographics
-# lexical_access_demo.csv
-# partID, LexTALE-ENG, LexTale-ESP, Bilingual Langauge Profile
-# Store in Box > Intuition > Active > data > attributes
-# Lexical Access Data by participant (only lab)
-# Store in Box > Laboratory > Active > data > input
- 
+# Write out file ready for analysis without heritage speakers
+write_csv(lexical_data_no_heritage, '42_lexical_access.csv')
+# For PI Advisor
+write_csv(lexical_data_no_heritage, 'data.csv') 
