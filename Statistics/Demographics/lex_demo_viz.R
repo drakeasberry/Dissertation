6bbdcf14ee9c084502331ff714b339e7e7717285
Drake Asberry <dasberry@email.arizona.edu>
2020-11-20 07:32:33 -0600

    added script file for lexical access experiment that may be revisited in the future

---
+++ Statistics/Demographics/lex_demo_viz.R
@@ -0,0 +1,23 @@
+#!/bin/bash
+#!/usr/bin/Rscript
+
+# Load Libraries
+library(plyr)
+library(readr)
+library(tidyverse)
+library(psych)
+library(lattice)
+library(tidyr)
+
+# Create 'not in' function
+'%ni%' <- Negate('%in%')
+
+# Soruce Scripts containing functions
+source("../../Scripts_Dissertation/segmentation_dataviz_script.R")
+
+# Read in data file
+vocab_size <- read_csv('analyze_data/output/lab_lex_vocab_sizes.csv')
+
+# Plot Language Vocabulary Difference in lab segmentation experiment
+all_demo <- vocab_diff_plt(vocab_size)
+all_demo
